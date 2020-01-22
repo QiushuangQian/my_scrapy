0dee22638621e74051a15f634509b7cf80713a00
QiushuangQian <qiushuangqian@sina.com>
2020-01-22 18:00:43 +0800

    ，第一页数据获取完成，下一页无法跳转

--- douban/douban/spiders/douban_spider.py
+++ douban/douban/spiders/douban_spider.py
@@ -14,11 +14,11 @@ class DoubanSpiderSpider(scrapy.Spider):
     # 默认解析方法
     def parse(self, response):
         movie_list = response.xpath("//div[@class='article']//ol[@class='grid_view']/li")
-        #循环电影条目
+        # 循环电影条目
         for i_item in movie_list:
-            #导入item文件
+            # 导入item文件
             douban_item = DoubanItem()
-            #写详细的xpath，进行数据解析
+            # 写详细的xpath，进行数据解析
             # /text() 以文本形式输出    extract()抽取
             douban_item['serial_number'] = i_item.xpath(".//div[@class='item']//em/text()").extract_first()
             douban_item['movie_name'] = i_item.xpath(
@@ -27,13 +27,14 @@ class DoubanSpiderSpider(scrapy.Spider):
             for i_content in content:
                 content_s = "".join(i_content.split())
                 douban_item['introduce'] = content_s
-            douban_item['star'] = i_item.xpath(".//span[@class='rating_num']/text()").extract_first()
-            douban_item['evaluate'] = i_item.xpath(".//div[@class='star‘]//span[4]/text()").extract_first()
-            douban_item['describe'] = i_item.xpath(".//p[@class='quote']/span/text()").extract_first()
-            #必须将数据yield到pipline中
+            douban_item['star'] = i_item.xpath(".//div[@class='bd']//div[@class='star']/span[2]/text()").extract_first()
+            douban_item['evaluate'] = i_item.xpath(
+                ".//div[@class='bd']//div[@class='star']/span[4]/text()").extract_first()
+            douban_item['describe'] = i_item.xpath(".//div[@class='bd']//p[@class='quote']/span[@class='inq']/text()").extract_first()
+            # 必须将数据yield到pipline中
             yield douban_item
-        #下一页请求，取后一页xpath
-        next_link = response.xpath("//span[@class='next']/link/@href").extract()
+        # 下一页请求，取后一页xpath
+        next_link = response.xpath(".//div[@class='paginator']/span[@class='next']/link/@href").extract()
         if next_link:
             next_link = next_link[0]
-            yield  scrapy.Request("https://movie.douban.com/top250"+next_link,callback=self.parse())
+            yield scrapy.Request("https://movie.douban.com/top250" + next_link, callback=self.parse())
